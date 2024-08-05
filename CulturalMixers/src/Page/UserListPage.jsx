a0e46bec0445e9ed1b79b52264d89d2979067b5a
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-05 16:11:37 +0900

    送金宛先画面の表示機能のUI

--- CulturalMixers/src/Page/UserListPage.jsx
+++ CulturalMixers/src/Page/UserListPage.jsx
@@ -1,9 +1,23 @@
-
+import { useEffect, useState } from "react";
+import { ListDummy } from "../Utils/dummyData";
+import ListDetail from "../Components/ListDetail";
+import "../List.css";
 
 export const UserListPage = () => {
-    return (
-        <>
-        <div>a</div>
-        </>
-    )
-}
+  const [getUser, setGetUser] = useState([]);
+
+  useEffect(() => {
+    setGetUser(ListDummy);
+  }, []);
+
+  return (
+    <>
+      <h2 className="SelectionUserTitle">送金相手を選択</h2>
+      <ul className="listcontainer">
+        {getUser.map((user, index) => (
+          <ListDetail key={index} user={user} />
+        ))}
+      </ul>
+    </>
+  );
+};
