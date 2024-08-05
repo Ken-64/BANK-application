3f07b7e09a2c93d3da60d487fd445ac85edc7dbc
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-05 15:09:00 +0900

    ダミーデータ生成

---
+++ CulturalMixers/src/Utils/dummyData.js
@@ -0,0 +1,33 @@
+// Dummy user
+export const UserDummy = [
+  {
+    User: {
+      account_number: "12345678",
+      money: 1000,
+      name: "フレンド",
+      icon: "human1.png",
+    },
+  },
+];
+
+// Dummy list
+export const ListDummy = [
+  {
+    User: {
+      name: "Aさん",
+      icon: "human2.png",
+    },
+  },
+  {
+    User: {
+      name: "Bさん",
+      icon: "human3.png",
+    },
+  },
+  {
+    User: {
+      name: "Cさん",
+      icon: "human4.png",
+    },
+  },
+];
