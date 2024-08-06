d2747d0931822d7050ed7bfa839a71f63903412a
Zazakizakizaki <keisan.yama20@gmail.com>
2024-08-06 10:50:32 +0900

    決済完了画面のCSSの修正

--- CulturalMixers/src/Page/SendMoneyFin.jsx
+++ CulturalMixers/src/Page/SendMoneyFin.jsx
@@ -4,7 +4,7 @@ export const SendMoneyFin = () => {
   return (
     <div className='FinishDisplay'>
       <img src="approval.png" alt="approval" height="200px"/>
-      <h2>送金完了</h2>
+      <h2>送金処理が完了しました</h2>
       <ReturnHomeButton />
     </div>
   );
