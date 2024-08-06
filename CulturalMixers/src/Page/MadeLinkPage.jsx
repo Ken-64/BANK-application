083c7a08a15ff195c6afb5635410a23e70d3fca7
Zazakizakizaki <keisan.yama20@gmail.com>
2024-08-06 11:26:51 +0900

    リンク作成ページの作成

---
+++ CulturalMixers/src/Page/MadeLinkPage.jsx
@@ -0,0 +1,14 @@
+import {ReturnHomeButton} from '../Components/ReturnHomeButton.js';
+import {MakeLinkButton} from '../Components/MakeLinkButton.js';
+
+export const MadeLinkFin = () => {
+
+  return (
+    <div className='FinishDisplay'>
+      <div className='MadeLinkMessage'>請求リンクが作成されました</div>
+      <div className='MadeLink'>請求リンク</div>
+      <MakeLinkButton />
+      <ReturnHomeButton />
+    </div>
+  );
+};
