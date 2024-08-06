e9de30000b36313bbbca8f074102298091421eb1
Ken-64 <hhzzjj110100@gmail.com>
2024-08-06 16:53:33 +0900

    page

---
+++ CulturalMixers/src/Page/Claimstatus.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import "../page.css";
+
+const Claimstatus = () => {
+  return (
+    <div className="ClaimstatusWrapper">
+      <p className="ClaimstatusMouth">2023年3月分</p>
+      <div className="ClaimstatusBorder">
+        <div className="ClaimstatusInner">
+          <p className="ClaimstatusDay">日付</p>
+          <p className="ClaimstatusMoney">お金</p>
+          <p className="ClaimstatusMessage">メッセージ</p>
+          <div className="payAndIconInner">
+            <p className="pay">支払い済み</p>
+            <img src="human6.png" alt="アイコン" width="200x" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Claimstatus;
