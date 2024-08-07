7feee4e6c0092d9bf232e66f1b5723299cb4805d
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-07 12:16:45 +0900

    fix

--- CulturalMixers/src/Page/Claimstatus.jsx
+++ CulturalMixers/src/Page/Claimstatus.jsx
@@ -1,21 +1,27 @@
 import React from "react";
 import "../page.css";
-import { BackToHomeButton } from "../Components/BackToHomeButton.js"
+import { BackToHomeButton } from "../Components/BackToHomeButton.js";
 const Claimstatus = () => {
   return (
     <div className="ClaimstatusWrapper">
       <p className="ClaimstatusMouth">2023年3月分</p>
       <div className="ClaimstatusBorder">
         <div className="ClaimstatusInner">
-          <p className="ClaimstatusDay">日付</p>
-          <p className="ClaimstatusMoney">お金</p>
-          <p className="ClaimstatusMessage">メッセージ</p>
+          <p className="ClaimstatusDay">日付:3月15日</p>
+          <p className="ClaimstatusMoney">
+            <b>支払額：</b>7800円
+          </p>
+          <p className="ClaimstatusMessage">
+            <b>メッセージ</b>
+            <br />
+            なし
+          </p>
           <div className="payAndIconInner">
             <p className="pay">支払い済み</p>
             <img src="human6.png" alt="アイコン" width="200x" />
           </div>
           <div className="BackToHome">
-            <BackToHomeButton/>
+            <BackToHomeButton />
           </div>
         </div>
       </div>
