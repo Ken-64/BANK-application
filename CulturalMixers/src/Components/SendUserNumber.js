d25024912e704a366685ddad4e0e51fdee8d6149
uni <147740687+shunmakk@users.noreply.github.com>
2024-08-07 10:51:54 +0900

    fix

--- CulturalMixers/src/Components/SendUserNumber.js
+++ CulturalMixers/src/Components/SendUserNumber.js
@@ -1,11 +1,26 @@
-import '..//App.css';
-
+import "..//App.css";
+import { useState, useEffect } from "react";
+import { getUserInfo } from "../API/api";
 
 export const SendUserNumber = () => {
-    return (
-        <>
-        <p className="SendUp">送金上限額 </p>
-        <p className="SendUPNumber">50,000円 </p>
-        </>
-    )
-}
+  const [userData, setUserData] = useState([]);
+
+  const userId = 1;
+  useEffect(() => {
+    (async function () {
+      try {
+        const userInfo = await getUserInfo(userId);
+        setUserData(userInfo);
+      } catch (error) {
+        console.error("エラー", error);
+      }
+    })();
+  }, []);
+
+  return (
+    <>
+      <p className="SendUp">送金上限額 </p>
+      <p className="SendUPNumber">{userData.balance}</p>
+    </>
+  );
+};
